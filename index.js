03698eb7031c27ce15fdfe8e910d7d0ed9f239e1
Vladimir Panov <vladimir.panov@motorsport.com>
2018-11-20 13:53:50 +0200

    Fixes rules duplucation problem. Added whitelist option to filter extractable properties

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const postcss = require('postcss');
 
 // holds resulting CSS
 const extractedContent = postcss.root();
+const existingRules = {};
 
 let currentAtRule = null;
 let currentRule = null;
@@ -13,6 +14,12 @@ let outputPaths = [];
 let filename = 'output';
 let ext = 'css';
 
+let totalFiles = 0;
+let processedFiles = 0;
+
+let ruleWithCustomPropertyRegExtp = /var\(\s*--/;
+let whiteListRegExp = null;
+
 function validateAndPrepareSettings(options) {
   if (!options) throw new Error('Options object is required');
 
@@ -20,6 +27,14 @@ function validateAndPrepareSettings(options) {
 
   if (typeof dist === 'string') {
     dist = [dist];
+  }
+
+  if (options.whitelist) {
+    if (Object.prototype.toString.call(options.whitelist) !== '[object Array]') {
+      throw new Error(`"whitelist" options should be an array, but you passed ${options.whitelist}`)
+    }
+
+    whiteListRegExp = new RegExp(`var\\(\\s*(?:(?:${options.whitelist.join(')|(?:')}))\\s*\\)`);
   }
 
   outputPaths = dist.map(curDist => path.resolve(process.cwd(), curDist));
@@ -34,66 +49,108 @@ function validateAndPrepareSettings(options) {
 }
 
 module.exports = postcss.plugin("postcss-reexport", function(options) {
+  currentAtRule = null;
+  currentRule = null;
+
   validateAndPrepareSettings(options);
+
+  totalFiles = options.totalCount;
   
   return function(styles, result) {
     styles.walkDecls(decl => {
       // we are only interested in rules that have custom properties
-      if (!decl.value.includes('var(--')) {
+      if (!ruleWithCustomPropertyRegExtp.test(decl.value)) {
         if (currentRule && currentRule.selector !== decl.parent.selector) {
           currentRule = null;
         }
         return;
       }
 
-      updateCurrentItems(decl);
+      if (whiteListRegExp) {
+        if (!whiteListRegExp.test(decl.value)) return;
+      }
+
+      if (updateCurrentItemsSkipExisting(decl)) return;
 
       currentRule.append(decl.clone());
     });
+
+    processedFiles++;
+
+    // do not output file if it's not the last file
+    if (totalFiles > processedFiles) return;
 
     outputPaths.forEach(path => {
       fs.outputFile(`${path}/${filename}.${ext}`, extractedContent.toString());
     })
   }
-});
 
-function isCurrentAtRuleChanged(target) {
-  return currentAtRule.name !== target.name || currentAtRule.params !== target.params
-}
+  function isCurrentAtRuleChanged(target) {
+    return currentAtRule.name !== target.name || currentAtRule.params !== target.params
+  }
 
-function updateCurrentItems(decl) {
-  const rule = decl.parent;
-  const atRule = rule.parent.type === 'atrule' ? rule.parent : null;
+  function getExistingRule(atRule, rule) {
+    let selector = '';
 
-  const noRuleOrChanged = !currentRule || currentRule.selector !== rule.selector;
-  const isAtRuleChanged = currentAtRule && (!atRule || isCurrentAtRuleChanged(atRule));
+    if (atRule) {
+      selector += `${atRule.name}_${atRule.params}-`;
+    }
+
+    selector += rule.selectors.join('_');
+
+    return existingRules[selector];
+  }
+
+  function ruleContainsDeclaration(rule, decl) {
+    return rule.some(def => def.prop === decl.prop && def.value === decl.value);
+  }
   
-  if (noRuleOrChanged || isAtRuleChanged) {
-    // create copy of the rule
-    currentRule = postcss.rule({
-      selector: rule.selector,
-      selectors: rule.selectors,
-    })
+  function updateCurrentItemsSkipExisting(decl) {
+    const rule = decl.parent;
+    const atRule = rule.parent.type === 'atrule' ? rule.parent : null;
+  
+    const existingRule = getExistingRule(atRule, rule);
 
-    if (!atRule) {
-      currentAtRule = null;
-      extractedContent.append(currentRule);
-    } else {
-      if (!currentAtRule) {
-        currentAtRule = postcss.atRule({
-          name: atRule.name,
-          params: atRule.params,
-        });
-        extractedContent.append(currentAtRule);
-      } else if (isCurrentAtRuleChanged(atRule)) {
-        currentAtRule = postcss.atRule({
-          name: atRule.name,
-          params: atRule.params,
-        });
-        extractedContent.append(currentAtRule);
+    if (existingRule) {
+      // we already have this rule in existingRules
+      if(ruleContainsDeclaration(existingRule, decl)) return true;
+
+      currentRule = existingRule;
+    }
+
+    const noRuleOrChanged = !currentRule || currentRule.selector !== rule.selector;
+    const isAtRuleChanged = currentAtRule && (!atRule || isCurrentAtRuleChanged(atRule));
+    
+    if (noRuleOrChanged || isAtRuleChanged) {
+      // create copy of the rule
+      currentRule = postcss.rule({
+        selector: rule.selector,
+        selectors: rule.selectors,
+      })
+  
+      if (!atRule) {
+        currentAtRule = null;
+        extractedContent.append(currentRule);
+        existingRules[currentRule.selectors.join('_')] = currentRule;
+      } else {
+        if (!currentAtRule) {
+          currentAtRule = postcss.atRule({
+            name: atRule.name,
+            params: atRule.params,
+          });
+          extractedContent.append(currentAtRule);
+        } else if (isCurrentAtRuleChanged(atRule)) {
+          currentAtRule = postcss.atRule({
+            name: atRule.name,
+            params: atRule.params,
+          });
+          extractedContent.append(currentAtRule);
+        }
+  
+        currentAtRule.append(currentRule);
+        const uniqueId = `${currentAtRule.name}_${currentAtRule.params}-${currentRule.selectors.join('_')}`
+        existingRules[uniqueId] = currentRule;
       }
-
-      currentAtRule.append(currentRule);
     }
   }
-}
+});
